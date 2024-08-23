4104026b9dadf5792be9251b8f660b2cdd1ef1d5
akandeseun <akandeseun44@gmail.com>
2024-08-23 09:04:42 +0100

    fix: upload function

--- controllers/r2.ts
+++ controllers/r2.ts
@@ -1,4 +1,5 @@
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { S3 } from 'aws-sdk';
 import fs from 'fs';
 import path from 'path';
 
@@ -8,7 +9,7 @@ export const uploadFile = async (bucketName: string, key: string, filePath: stri
     endpoint: `https://${process.env.ACCOUNT_ID}.r2.cloudflarestorage.com`,
     credentials: {
       accessKeyId: process.env.ACCESS_KEY_ID!,
-      secretAccessKey: process.env.SECRET_ACCESS_KEY!,
+      secretAccessKey: process.env.SECRET_ACCESS_KEY!
     }
   });
 
@@ -18,8 +19,8 @@ export const uploadFile = async (bucketName: string, key: string, filePath: stri
     Bucket: bucketName,
     Key: key,
     ContentType: 'application/x-gzip',
-    Body: fileStream,
-  }
+    Body: fileStream
+  };
 
   try {
     console.log('Sending upload command');
@@ -32,5 +33,34 @@ export const uploadFile = async (bucketName: string, key: string, filePath: stri
     }
   } finally {
     fileStream.destroy(); // Close the file stream
+  }
+};
+
+export const uploadR2 = async (bucketName: string, fileName: string, filePath: string) => {
+  const r2 = new S3({
+    accessKeyId: process.env.R2_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!,
+    endpoint: process.env.R2_ENDPOINT!,
+    region: 'auto',
+    s3ForcePathStyle: false,
+    signatureVersion: 'v4'
+  });
+
+  const fileStream = fs.createReadStream(filePath);
+
+  const params = {
+    Bucket: bucketName,
+    Key: fileName,
+    Body: fileStream,
+    ContentType: 'application/x-gzip'
+  };
+
+  try {
+    const data = await r2.upload(params).promise();
+    console.log(`File uploaded successfully. R2 URL: ${data.Location}`);
+  } catch (error) {
+    console.log(`Error uploading: ${error}`);
+  } finally {
+    fileStream.destroy();
   }
 };
