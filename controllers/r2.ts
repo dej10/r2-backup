ce43f536f0704ecdd4c2f4beeef94dec87ff051e
akandeseun <akandeseun44@gmail.com>
2024-10-03 19:02:35 +0100

    feat: delete backups after two months

--- controllers/r2.ts
+++ controllers/r2.ts
@@ -1,19 +1,18 @@
 import { Upload } from '@aws-sdk/lib-storage';
-import { S3 } from '@aws-sdk/client-s3';
+import { DeleteObjectCommand, ListObjectsV2Command, S3 } from '@aws-sdk/client-s3';
 import fs, { unlink } from 'fs';
 
+const r2 = new S3({
+  credentials: {
+    accessKeyId: process.env.R2_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!
+  },
+  endpoint: process.env.R2_ENDPOINT!,
+  region: 'auto',
+  forcePathStyle: false
+});
 
 export const uploadR2 = async (bucketName: string, fileName: string, filePath: string) => {
-  const r2 = new S3({
-    credentials: {
-      accessKeyId: process.env.R2_ACCESS_KEY_ID!,
-      secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!
-    },
-    endpoint: process.env.R2_ENDPOINT!,
-    region: 'auto',
-    forcePathStyle: false,
-  });
-
   const fileStream = fs.createReadStream(filePath);
 
   const params = {
@@ -31,19 +30,51 @@ export const uploadR2 = async (bucketName: string, fileName: string, filePath: s
     }).done();
     console.log(`File uploaded successfully. R2 URL: ${data.Location}`);
 
-    // delete file after uploading to R2 
-     unlink(filePath, (err) => {
+    // delete file after uploading to R2
+    unlink(filePath, (err) => {
       if (err) {
         console.error('Error deleting file:', err);
       } else {
         console.log('File deleted successfully');
       }
     });
-
-
   } catch (error) {
     console.log(`Error uploading: ${error}`);
   } finally {
     fileStream.destroy();
+  }
+};
+
+export const deleteOldBackups = async () => {
+  console.log('Checking for outdated backups...');
+  const bucketName = process.env.R2_BUCKET_NAME!;
+  const twoMonthsAgo = new Date();
+  twoMonthsAgo.setMonth(twoMonthsAgo.getMonth() - 2);
+
+  try {
+    const listParams = {
+      Bucket: bucketName
+    };
+
+    const listCommand = new ListObjectsV2Command(listParams);
+    const data = await r2.send(listCommand);
+
+    if (data.Contents) {
+      for (const object of data.Contents) {
+        if (object.LastModified && object.LastModified < twoMonthsAgo) {
+          const deleteParams = {
+            Bucket: bucketName,
+            Key: object.Key
+          };
+
+          const deleteCommand = new DeleteObjectCommand(deleteParams);
+          await r2.send(deleteCommand);
+          console.log(`Deleted old backup: ${object.Key}`);
+        }
+      }
+    }
+    console.log('Cleanup of old backups completed successfully.');
+  } catch (error) {
+    console.error('Error during cleanup of old backups:', error);
   }
 };
